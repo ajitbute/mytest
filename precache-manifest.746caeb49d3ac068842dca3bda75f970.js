d4bbb3dda729bae432e8475e9d34936961451ba7
ajitbute <ajitbute@gmail.com>
2020-07-15 12:29:39 -0700

    Updates

---
+++ precache-manifest.746caeb49d3ac068842dca3bda75f970.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15b13e68c1d7be148ef2337265574e4",
+    "url": "/mytest/index.html"
+  },
+  {
+    "revision": "1f2ad2c039a9edb1349b",
+    "url": "/mytest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/mytest/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mytest/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2ad2c039a9edb1349b",
+    "url": "/mytest/static/js/main.10ef8cf3.chunk.js"
+  },
+  {
+    "revision": "13af5518f6f0bdcff358",
+    "url": "/mytest/static/js/runtime-main.00dd5e95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mytest/static/media/logo.5d5d9eef.svg"
+  }
+]);
